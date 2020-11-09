d29c1cbb4f557f25c8034f2f3d920648597bf60e
FrancescoDale <fd060862@gmail.com>
2020-11-09 14:19:34 +0100

    immagini e ridimensionamento

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ var app = new Vue {
     data : {
         imageIndex : 0,
         pics : [
-            
+            'https://uozzart.com/wp-content/uploads/2019/07/Campo-de-fiori.jpg',
+            'https://lh3.googleusercontent.com/proxy/oCybApm2jvTAzTDao4xQH0L2Bemv2hliMU3zRyjvw13gSVWNd2sgm5GVZc9W_g2s05pyqTHPtAbmgqtSEoG8rYJHKg8PTd0HFhMcMufz479OFhVwAncMs-JTs59q3AOWvCV8qA',
+            'https://st.ilfattoquotidiano.it/wp-content/uploads/2020/04/21/roma1200-1-690x362.jpg',
+            'https://www.ilfaroonline.it/photogallery_new/images/2020/03/campidoglio-121445.660x368.jpg',
         ],
 
     },
